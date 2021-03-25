c6f5cb5a263abd74d10e0e571924415ba93a7d0b
diegocb81 <diegocb81@gmail.com>
2021-03-25 15:24:24 -0300

    Finalizando o código e desenvolvendo novas frases

--- app/src/main/java/com/cursoandroid/projeto_frases_do_dia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/projeto_frases_do_dia/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,8 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void gerarNovaFrase(View view){
 
+        String[] frases = {
+                "Um pequeno pensamento positivo pela manhã pode mudar todo o seu dia!",
+                "Pra hoje: sorrisos bobos, uma mente tranquila e um coração cheio de paz!",
+                "Que o dia seja leve, que a tristeza seja breve e que o dia seja feliz!",
+                "Sempre que o sol nasce, você tem uma oportunidade de ser feliz!"
+        };
+        int numero = new Random().nextInt(4);
+
         TextView texto = findViewById(R.id.textResultado);
-        texto.setText("Diego");
+        texto.setText(frases[numero]);
     }
 
 }
