052efa0dec0502c737a4708128de32a189bd80fa
diegocb81 <diegocb81@gmail.com>
2021-03-25 07:57:44 -0300

    Gerando nova frase

--- app/src/main/java/com/cursoandroid/projeto_frases_do_dia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/projeto_frases_do_dia/MainActivity.java
@@ -3,6 +3,8 @@ package com.cursoandroid.projeto_frases_do_dia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText("Diego");
+    }
+
 }
